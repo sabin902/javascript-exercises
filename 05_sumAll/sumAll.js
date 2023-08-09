d2c2cc9e8f187b68db9e637586280332a873391b
sabin902 <124549107+sabin902@users.noreply.github.com>
2023-08-09 13:20:22 -0400

    #

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0;
+    for(let i = start;i <= end;i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+    
+  });
+  return sum;
 	
 };
 
-const sum = function() {
+const multiply = function(array) {
+  if (array.length < 1){
+    return 0
+
+  }
+  let product = 1;
+  array.forEach(element => {
+    product *= element;
+    
+  });
+  return product;
+
+
+};
+
+const power = function(a,b) {
+  let power = 1;
+  for(let i = 1;i <= b;i++){
+    power *= a;
+
+  }
+  return power;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(a) {
+  let factorial =  1;
+  for (let i = a;i >= 1; i--){
+    factorial *= i;
+  }
+  return factorial;
 	
 };
 
