f63365205bd52d91da047cbcb3c3557cae55af0c
sabin902 <124549107+sabin902@users.noreply.github.com>
2023-08-17 12:20:47 -0400

    #

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
 const palindromes = function (word) {
+    let altered_wrd = word.replace(/[^\w\s]/g, '').replace(/\s/g, '').toLowerCase();
+    let new_wrd = ""
+    for(let i = altered_wrd.length-1;i >= 0;i--){
+        new_wrd += altered_wrd[i];
+    }
+    if (new_wrd == altered_wrd){
+        return true;
+    }else{
+        return false;
+    }
 
 };
 
